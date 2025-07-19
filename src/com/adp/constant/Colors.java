8f50b662dbb82bcfd277ea14852c076e34a79d6b
Chamod Dilshan <chamoddilshan834@gmail.com>
2025-07-19 22:12:18 +0530

    Dashboard Panel & Constant Package impliment

---
+++ src/com/adp/constant/Colors.java
@@ -0,0 +1,9 @@
+package com.adp.constant;
+
+import java.awt.Color;
+
+public class Colors {
+
+    public static final Color PRIMARAY = new Color(244, 244, 244);
+    public static final Color BORDER = new Color(227, 227, 227);
+}
